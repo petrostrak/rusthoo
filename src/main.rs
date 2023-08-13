18ca4ebdd280c469f9898815a396d926d2e397cb
petrostrak <pit.trak@gmail.com>
2023-08-13 17:42:03 +0300

    add next_token and trim_left lexer methods

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,22 @@ impl<'a> Lexer<'a> {
     fn new(content: &'a [char]) -> Self {
         Self { content }
     }
+
+    fn trim_left(&mut self) {
+        // trim whitespaces from the left
+        while self.content.len() > 0 && self.content[0].is_whitespace() {
+            self.content = &self.content[1..];
+        }
+    }
+
+    fn next_token(&mut self) -> Option<&'a [char]> {
+        // trim whitespaces from the left
+        self.trim_left();
+        if self.content.len() == 0 {
+            return None;
+        }
+        todo!()
+    }
 }
 
 fn index_document(content: &str) -> HashMap<String, usize> {
