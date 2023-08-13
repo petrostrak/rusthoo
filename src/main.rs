bf0f8019a2c6496458e2e1a6340d9835f0e8344f
petrostrak <pit.trak@gmail.com>
2023-08-13 17:23:56 +0300

    add Lexer struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 use std::{
+    collections::HashMap,
     fs::{read_dir, File},
     io,
     path::Path,
 };
 use xml::reader::{EventReader, XmlEvent};
+
+#[derive(Debug)]
+struct Lexer<'a> {
+    content: &'a [char],
+}
+
+impl<'a> Lexer<'a> {
+    fn new(content: &'a [char]) -> Self {
+        Self { content }
+    }
+}
+
+fn index_document(content: &str) -> HashMap<String, usize> {
+    todo!("not implemented")
+}
 
 fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
     let file = File::open(file_path)?;
@@ -19,14 +35,21 @@ fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
 }
 
 fn main() -> io::Result<()> {
-    let dir_path = "docs.gl/gl4";
-    let dir = read_dir(dir_path)?;
+    let content = read_entire_xml_file("docs.gl/gl4/glBeginQuery.xhtml")?
+        .chars()
+        .collect::<Vec<_>>();
 
-    for file in dir {
-        let file_path = file?.path();
-        let content = read_entire_xml_file(&file_path)?;
-        println!("{file_path:?} => {size}", size = content.len());
-    }
+    let lexer = Lexer::new(&content);
+    println!("{lexer:?}");
+
+    // let dir_path = "docs.gl/gl4";
+    // let dir = read_dir(dir_path)?;
+
+    // for file in dir {
+    //     let file_path = file?.path();
+    //     let content = read_entire_xml_file(&file_path)?;
+    //     println!("{file_path:?} => {size}", size = content.len());
+    // }
 
     Ok(())
 }
