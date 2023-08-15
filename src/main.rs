cf702762560ca6cda8513f9d550b9b6628c13c05
petrostrak <pit.trak@gmail.com>
2023-08-15 10:36:34 +0300

    introduce the notion of subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::{
     collections::HashMap,
+    env,
     fs::{read_dir, File},
     io,
     path::{Path, PathBuf},
+    process::ExitCode,
 };
 use xml::reader::{EventReader, XmlEvent};
 
@@ -66,10 +68,6 @@ impl<'a> Iterator for Lexer<'a> {
     }
 }
 
-fn index_document(content: &str) -> TermFreq {
-    todo!("not implemented")
-}
-
 fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
     let file = File::open(file_path)?;
     let event_reader = EventReader::new(file);
@@ -84,15 +82,30 @@ fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
     Ok(content)
 }
 
-type TermFreq = HashMap<String, usize>;
-type TermFreqIndex = HashMap<PathBuf, TermFreq>;
+fn save_tf_index(tf_index: &TermFreqIndex, index_path: &str) -> Result<(), ()> {
+    println!("Saving {index_path}...");
+    let index_file = File::create(index_path).unwrap();
 
-fn main() -> io::Result<()> {
-    let index_path = "index.json";
-    let index_file = File::open(index_path)?;
+    let json = serde_json::to_string(&tf_index).or_else(|err| {
+        return Err(());
+    });
+
+    serde_json::to_writer_pretty(index_file, &json).or_else(|err| {
+        return Err(());
+    });
+
+    for (path, tf) in tf_index {
+        println!("{path:?} has {count} unique tokens", count = tf.len())
+    }
+
+    Ok(())
+}
+
+fn check_index(index_path: &str) -> Result<(), ()> {
+    let index_file = File::open(index_path).unwrap();
     println!("Reading {index_path} index file...");
 
-    let tf_index: String = serde_json::from_reader(index_file)?;
+    let tf_index: String = serde_json::from_reader(index_file).unwrap();
     println!(
         "{index_path} contains {count} files",
         count = tf_index.len()
@@ -100,54 +113,99 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-// fn main() -> io::Result<()> {
-//     let dir_path = "docs.gl/gl4";
-//     let dir = read_dir(dir_path)?;
-//     let top_n = 20;
-//     let mut tf_index = TermFreqIndex::new();
+fn tf_index_of_folder(dir_path: &str, tf_index: &mut TermFreqIndex) -> Result<(), ()> {
+    let dir = read_dir(dir_path).unwrap();
+    let top_n = 20;
 
-//     for file in dir {
-//         let file_path = file?.path();
+    for file in dir {
+        let file_path = file.unwrap().path();
 
-//         println!("Indexing {file_path:?}...");
+        println!("Indexing {file_path:?}...");
 
-//         let content = read_entire_xml_file(&file_path)?
-//             .chars()
-//             .collect::<Vec<_>>();
+        let content = read_entire_xml_file(&file_path)
+            .unwrap()
+            .chars()
+            .collect::<Vec<_>>();
 
-//         let mut table_freq = TermFreq::new();
+        let mut table_freq = TermFreq::new();
 
-//         for token in Lexer::new(&content) {
-//             let term = token
-//                 .iter()
-//                 .map(|c| c.to_ascii_uppercase())
-//                 .collect::<String>();
+        for token in Lexer::new(&content) {
+            let term = token
+                .iter()
+                .map(|c| c.to_ascii_uppercase())
+                .collect::<String>();
 
-//             if let Some(freq) = table_freq.get_mut(&term) {
-//                 *freq += 1;
-//             } else {
-//                 table_freq.insert(term, 1);
-//             };
-//         }
+            if let Some(freq) = table_freq.get_mut(&term) {
+                *freq += 1;
+            } else {
+                table_freq.insert(term, 1);
+            };
+        }
 
-//         let mut stats = table_freq.iter().collect::<Vec<_>>();
-//         stats.sort_by_key(|f| f.1);
-//         stats.reverse();
+        let mut stats = table_freq.iter().collect::<Vec<_>>();
+        stats.sort_by_key(|f| f.1);
+        stats.reverse();
 
-//         tf_index.insert(file_path, table_freq);
-//     }
+        tf_index.insert(file_path, table_freq);
+    }
 
-//     let index_path = "index.json";
-//     println!("Saving {index_path}...");
-//     let index_file = File::create(index_path)?;
+    Ok(())
+}
 
-//     let json = serde_json::to_string(&tf_index)?;
+type TermFreq = HashMap<String, usize>;
+type TermFreqIndex = HashMap<PathBuf, TermFreq>;
 
-//     serde_json::to_writer_pretty(index_file, &json)?;
+fn usage(program: &str) {
+    eprintln!("Usage: {program} [SUBCOMMAND] [OPTIONS]");
+    eprintln!("Subcommands:");
+    eprintln!("    index <folder> [address]       index the <folder> and seva the index to index.json file");
+    eprintln!(
+        "    search <index-file> [address]       check how many documents are indexed in the file"
+    );
+}
 
-//     for (path, tf) in tf_index {
-//         println!("{path:?} has {count} unique tokens", count = tf.len())
-//     }
+fn entry() -> Result<(), ()> {
+    let mut args = env::args();
+    let program = args.next().expect("path to program is provided");
 
-//     Ok(())
-// }
+    let subcommand = args.next().ok_or_else(|| {
+        usage(&program);
+        eprintln!("ERROR: no subcommand is provided");
+    })?;
+
+    match subcommand.as_str() {
+        "index" => {
+            let dir_path = args.next().ok_or_else(|| {
+                usage(&program);
+                eprintln!("ERROR: no directory provided for {subcommand} subcommand");
+            })?;
+
+            let mut tf_index = TermFreqIndex::new();
+            tf_index_of_folder(&dir_path, &mut tf_index)?;
+            save_tf_index(&tf_index, "index.json")?;
+        }
+        "search" => {
+            let index_path = args.next().ok_or_else(|| {
+                usage(&program);
+                eprintln!("ERROR: no path to index is provided for {subcommand} subcommand");
+            })?;
+
+            check_index(&index_path)?;
+        }
+
+        _ => {
+            usage(&program);
+            eprintln!("ERROR: unknown subcommand {subcommand}");
+            return Err(());
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> ExitCode {
+    match entry() {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(()) => ExitCode::FAILURE,
+    }
+}
